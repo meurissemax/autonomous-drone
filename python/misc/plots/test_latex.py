8215142f0f95ee0eb5c16cde3bbcc960c2c83163
Maxime Meurisse <max.meurisse@outlook.com>
2021-03-13 12:23:12 +0100

    ✅ Add test scripts
    * Plot with LaTeX settings
    * Simple trajectory for controllers

---
+++ python/misc/plots/test_latex.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+"""
+Implementation of some tests with settings used to generate plots with LaTeX
+style.
+"""
+
+###########
+# Imports #
+###########
+
+import numpy as np
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(os.path.dirname(current))
+sys.path.append(parent)
+
+from plots.latex import plt  # noqa: E402
+
+
+#####################
+# General variables #
+#####################
+
+FUNCTIONS = {
+    'sin': np.sin,
+    'cos': np.cos,
+    'tan': np.tan,
+    'arcsin': np.arcsin,
+    'arccos': np.arccos,
+    'arctan': np.arctan,
+    'exp': np.exp,
+    'log': np.log
+}
+
+
+########
+# Main #
+########
+
+def main(
+    start: int = 1,
+    stop: int = 100,
+    step: float = 0.1,
+    function_id: str = 'sin'
+):
+    # Function
+    f = FUNCTIONS.get(function_id, 'sin')
+
+    # Values
+    x = np.arange(start, stop, step)
+    y = f(x)
+
+    # Figure
+    plt.figure('Test')
+
+    plt.plot(x, y)
+
+    plt.xlabel('x')
+    plt.ylabel(r'$f(x)$')
+
+    plt.legend([function_id])
+
+    plt.show()
+    plt.close()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Generate a test plot with LaTeX plot settings.'
+    )
+
+    parser.add_argument(
+        '-start',
+        type=int,
+        default=1,
+        help='minimum value of x'
+    )
+
+    parser.add_argument(
+        '-stop',
+        type=int,
+        default=100,
+        help='maximum value of x'
+    )
+
+    parser.add_argument(
+        '-step',
+        type=float,
+        default=0.1,
+        help='step between each consecutive x value'
+    )
+
+    parser.add_argument(
+        '-function',
+        type=str,
+        default='sin',
+        choices=list(FUNCTIONS.keys()),
+        help='function to plot'
+    )
+
+    args = parser.parse_args()
+
+    main(
+        start=args.start,
+        stop=args.stop,
+        step=args.step,
+        function_id=args.function
+    )
---
+++ python/misc/uav/test_controllers.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+"""
+Implementation of some tests with controllers.
+"""
+
+###########
+# Imports #
+###########
+
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(os.path.dirname(current))
+sys.path.append(parent)
+
+from uav.controllers import (  # noqa: E402
+    AirSimDrone,
+    AirSimDroneNoisy,
+    Controller,
+    TelloEDU
+)
+
+
+#############
+# Functions #
+#############
+
+def trajectory(controller: Controller):
+    # Initialize the drone
+    controller.arm()
+    controller.takeoff()
+
+    # Follow simple trajectory
+    for _ in range(4):
+        controller.move('forward', 100, 50)
+        controller.rotate('cw', 180)
+
+    # When drone exits manual control, stop it
+    controller.land()
+    controller.disarm()
+
+
+########
+# Main #
+########
+
+def main(
+    controller_id: str = 'airsim'
+):
+    # Controller
+    controllers = {
+        'airsim': AirSimDrone,
+        'noisy': AirSimDroneNoisy,
+        'telloedu': TelloEDU
+    }
+
+    controller = controllers.get(controller_id)()
+
+    # Trajectory
+    trajectory(controller)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Simple test trajectory with controllers.'
+    )
+
+    parser.add_argument(
+        '-c',
+        '--controller',
+        type=str,
+        default='airsim',
+        choices=['airsim', 'noisy', 'telloedu'],
+        help='choice of the controller to use'
+    )
+
+    args = parser.parse_args()
+
+    main(
+        controller_id=args.controller
+    )
